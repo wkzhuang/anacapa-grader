29e1f480614d2df37145f3e288134999a027a626
wkzhuang <jameszwk9596@gmail.com>
2016-02-22 22:10:38 -0800

    remove useless files for [organizations] model in /spec

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -1,6 +1,6 @@
 class UserController < ApplicationController
       def index
-      	  client = Octokit::Client.new(access_token: "fed018790923f6eb9cc36e58c3246242baad57c6") 
+      	  client = Octokit::Client.new(access_token: "12b70ab44d9c507bdee5246bc0b926d79c979116") 
           @organizations = client.organization_memberships
       end
 end
